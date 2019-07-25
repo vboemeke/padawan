471da112cd415d226d4eff93ab4b779c013b0a28
Lucas Moresco <lucas.pmoresco@gmail.com>
2019-07-25 02:12:31 -0300

    Implements our first class

--- db/database.py
+++ db/database.py
@@ -2,88 +2,86 @@ import sqlite3
 import datetime
 import os
 
-data_dir = './data'
-logs_dir = './data/logs'
 
+class Database:
 
-def create_tables():
-    exchanges = ['kraken', 'bittrex', 'bitmex', 'bitfinex', 'bitstamp', 'okcoin', 'binance', 'coss', 'kucoin', 'poloniex', 'theocean', 'upbit']
+    def __init__(self):
+        self.__data_dir = './data'
+        self.__logs_dir = './data/logs'
+        self.__db_path = './data/padawan.db'
+        self.__create_log_path()
 
-    create_attempt_table()
+        self.__conn = sqlite3.connect(self.__db_path)
+        self.__cursor = self.__conn.cursor()
+        self.__create_tables()
 
-    for exchange in exchanges:
-        create_exchange_table(exchange)
+    def __del__(self):
+        if self.__conn:
+            self.__conn.close()
 
+    def __create_log_path(self):
+        if not os.path.exists(self.__data_dir):
+            os.makedirs(self.__data_dir)
 
-def create_log_path():
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
+        if not os.path.exists(self.__logs_dir):
+            os.makedirs(self.__logs_dir)
 
-    if not os.path.exists(logs_dir):
-        os.makedirs(logs_dir)
+    def __create_tables(self):
+        exchanges = ['kraken', 'bittrex', 'bitmex', 'bitfinex', 'bitstamp', 'okcoin', 'binance', 'coss', 'kucoin',
+                     'poloniex', 'theocean', 'upbit']
 
+        self.__create_attempt_table()
 
-def create_exchange_table(exchange_name):
-    db = sqlite3.connect('./data/padawan.db')
-    cursor = db.cursor()
+        for exchange in exchanges:
+            self.__create_exchange_table(exchange)
 
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS {table_name} ('
-        ' id INTEGER PRIMARY KEY AUTOINCREMENT,'
-        ' created_at DATETIME NOT NULL,'
-        ' bid DECIMAL(8, 2),'
-        ' bid_volume DECIMAL(8, 2),'
-        ' ask DECIMAL(8, 2),'
-        ' ask_volume DECIMAL(8, 2),'
-        ' attempt_id INTEGER,'
-        ' CONSTRAINT fk_attempts'
-        '   FOREIGN KEY (attempt_id)'
-        '   REFERENCES attempts(id))'.format(table_name=exchange_name))
+    def __create_exchange_table(self, exchange_name):
 
-    db.commit()
-    db.close()
+        self.__cursor.execute(
+            'CREATE TABLE IF NOT EXISTS {table_name} ('
+            ' id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            ' created_at DATETIME NOT NULL,'
+            ' bid DECIMAL(8, 2),'
+            ' bid_volume DECIMAL(8, 2),'
+            ' ask DECIMAL(8, 2),'
+            ' ask_volume DECIMAL(8, 2),'
+            ' attempt_id INTEGER,'
+            ' CONSTRAINT fk_attempts'
+            '   FOREIGN KEY (attempt_id)'
+            '   REFERENCES attempts(id))'.format(table_name=exchange_name))
 
+        self.__conn.commit()
 
-def create_attempt_table():
-    db = sqlite3.connect('./data/padawan.db')
-    cursor = db.cursor()
+    def __create_attempt_table(self):
 
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS attempts ('
-        ' id INTEGER PRIMARY KEY AUTOINCREMENT,'
-        ' created_at DATETIME NOT NULL,'
-        ' actual_balance DECIMAL(8, 2),'
-        ' target_entry_spread bid DECIMAL(8, 2),'
-        ' profit_target bid DECIMAL(8, 2))')
+        self.__cursor.execute(
+            'CREATE TABLE IF NOT EXISTS attempts ('
+            ' id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            ' created_at DATETIME NOT NULL,'
+            ' actual_balance DECIMAL(8, 2),'
+            ' target_entry_spread bid DECIMAL(8, 2),'
+            ' profit_target bid DECIMAL(8, 2))')
 
-    db.commit()
-    db.close()
+        self.__conn.commit()
 
+    def add_bid_ask_to_db(self, exchange_name, bid, bid_volume, ask, ask_volume, attempt):
 
-def add_bid_ask_to_db(exchange_name, bid, bid_volume, ask, ask_volume, attempt):
-    db = sqlite3.connect('./data/padawan.db')
-    cursor = db.cursor()
+        self.__cursor.execute('''INSERT INTO {table_name}(created_at, bid, bid_volume, ask, ask_volume, attempt_id)
+                          VALUES(?,?,?,?,?,?)'''.format(table_name=exchange_name),
+                              (datetime.datetime.now(), bid, bid_volume,
+                               ask, ask_volume, attempt))
 
-    cursor.execute('''INSERT INTO {table_name}(created_at, bid, bid_volume, ask, ask_volume, attempt_id)
-                      VALUES(?,?,?,?,?,?)'''.format(table_name=exchange_name),
-                   (datetime.datetime.now(), bid, bid_volume,
-                    ask, ask_volume, attempt))
-    db.commit()
-    db.close()
+        self.__conn.commit()
 
+    def new_attempt(self, actual_balance, target_entry_spread, profit_target):
 
-def new_attempt(actual_balance, target_entry_spread, profit_target):
-    db = sqlite3.connect('./data/padawan.db')
-    cursor = db.cursor()
+        self.__cursor.execute('''INSERT INTO attempts(created_at,
+                            actual_balance,
+                            target_entry_spread,
+                            profit_target)
+                          VALUES(?,?,?,?)''',
+                              (datetime.datetime.now(), actual_balance, target_entry_spread, profit_target))
 
-    cursor.execute('''INSERT INTO attempts(created_at,
-                        actual_balance,
-                        target_entry_spread,
-                        profit_target)
-                      VALUES(?,?,?,?)''',
-                   (datetime.datetime.now(), actual_balance, target_entry_spread, profit_target))
+        self.__conn.commit()
 
-    db.commit()
-    db.close()
-
-    return cursor.lastrowid
+        return self.__cursor.lastrowid
