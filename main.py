0ec3bd669ecaf911b0c0f50b1c6cdf2fae231ce3
Lucas Moresco <lucas.pmoresco@gmail.com>
2019-07-27 22:03:47 -0300

    Generate data frame only on trade exchanges already start

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ global book_df
 db = Database()
 
 
-def generate_dataframe():
+def generate_dataframe(exchange_list):
     dict = {}
     list_of_books = []
 
     # TODO increase exchange list (validate symbols 'BTC/USC' doesn't work on other exchanges
     # but we need to check if is the same coin - ex. Dolar vs. Stable Dolar)
 
-    for exchange_name in exchanges.exchange_list:
+    for exchange_name in exchange_list:
         exchange_data = exchanges.fetch_exchange_data(exchange_name)
         if exchange_data != {}:
             dict[exchange_name] = exchange_data
@@ -120,7 +120,13 @@ logger = Logger(attempt, target_entry_spread, profit_target)
 logger.run('✅💰 Saldo inicial: US$ %f 💰✅' % actual_balance)
 
 while 1 < 2:
-    book_df = generate_dataframe()
+    if not in_trade:
+        book_df = generate_dataframe(exchanges.exchange_list)
+    else:
+        book_df = generate_dataframe([trade_info[0][1], trade_info[1][1]])
+
+    # TODO generate trade_info data_frame
+
     opportunity = best_opportunity()
 
     logger.run('###############################')
@@ -139,20 +145,21 @@ while 1 < 2:
     if not in_trade:
         print('Spread atual - taxas e lucro:  %f' % (entry_spread - profit_target - (
                 2.0 * (exchanges.exchange_fees[opportunity[4]] + exchanges.exchange_fees[opportunity[5]]))))
-        if (entry_spread > target_entry_spread) & ((entry_spread - profit_target - (
-                2.0 * (
-                exchanges.exchange_fees[opportunity[4]] + exchanges.exchange_fees[opportunity[5]]))) > 1.00) & (
-                selling_exchange != buying_exchange):
-            entry_volume = float(opportunity[3])
-            logger.run('💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰')
-            logger.run('Entrando na operação com spread: %f e volume: %f BTC (0.5x do volume do menor book)' % (
-                entry_spread, entry_volume))
-            logger.run('💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰')
-            # creating traded spread to calculate exit_spread_target
-            traded_spread = entry_spread
-            operation = simulate_trade(opportunity)
-            temp_balance = operation[0][2] + operation[1][2]
-            in_trade = True
+        if 0 == 0:
+            if (entry_spread > target_entry_spread) & ((entry_spread - profit_target - (
+                    2.0 * (
+                    exchanges.exchange_fees[opportunity[4]] + exchanges.exchange_fees[opportunity[5]]))) > 1.00) & (
+                    selling_exchange != buying_exchange):
+                entry_volume = float(opportunity[3])
+                logger.run('💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰')
+                logger.run('Entrando na operação com spread: %f e volume: %f BTC (0.5x do volume do menor book)' % (
+                    entry_spread, entry_volume))
+                logger.run('💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰')
+                # creating traded spread to calculate exit_spread_target
+                traded_spread = entry_spread
+                trade_info = simulate_trade(opportunity)
+                temp_balance = trade_info[0][2] + trade_info[1][2]
+                in_trade = True
         else:
             logger.run(
                 '🏃 ‍Jumping - Current Spread %f < Target Spread %f. ###### Selling %s X Buying %s ###### 🏃' %
@@ -161,8 +168,8 @@ while 1 < 2:
     else:
         logger.run('Trying to close the transaction...')
 
-        actual_sell_price_bought_exchange = book_df[book_df['Exchange'] == operation[0][1]]['Bid Price'].values[0]
-        actual_buy_price_sold_exchange = book_df[book_df['Exchange'] == operation[0][1]]['Ask Price'].values[0]
+        actual_sell_price_bought_exchange = book_df[book_df['Exchange'] == trade_info[0][1]]['Bid Price'].values[0]
+        actual_buy_price_sold_exchange = book_df[book_df['Exchange'] == trade_info[0][1]]['Ask Price'].values[0]
         # manual calculating spread because actual spread is considering buy again what was already bought...
         real_actual_spread = actual_buy_price_sold_exchange / actual_sell_price_bought_exchange
         logger.run('Real Spread: %f' % real_actual_spread)
@@ -171,23 +178,23 @@ while 1 < 2:
         actual_spread = spread_of(selling_exchange, buying_exchange) - 1
         # logger.run('ACTUAL SPREAD %f' % actual_spread)
         exit_spread_target = traded_spread - profit_target - (
-                2.0 * (exchanges.exchange_fees[operation[0][1]] + (exchanges.exchange_fees[operation[1][1]])))
+                2.0 * (exchanges.exchange_fees[trade_info[0][1]] + (exchanges.exchange_fees[trade_info[1][1]])))
         logger.run('Spread de entrada - spread atual: %f' % (entry_spread - real_actual_spread))
         logger.run('Spread target para sair: %f' % exit_spread_target)
         if exit_spread_target >= real_actual_spread:
             # TODO: Considerar a taxa e volume inicial
-            profit = temp_balance + ((actual_sell_price_bought_exchange * operation[0][3]) - (
-                    actual_buy_price_sold_exchange * operation[0][3]))
+            profit = temp_balance + ((actual_sell_price_bought_exchange * trade_info[0][3]) - (
+                    actual_buy_price_sold_exchange * trade_info[0][3]))
             actual_balance += profit
             logger.run('\n*** Concluindo transação ***\n')
             logger.run(' spread de entrada %f \n spread de saida: %f \n lucro US$ %f' % (
                 opportunity[0], actual_spread, profit))
 
             # checking available exit volume:
-            if opportunity[3] < operation[0][3]:
+            if opportunity[3] < trade_info[0][3]:
                 logger.run(
                     '🚨 Atenção: volume de saída menor do que o de entrada! Entrada: %f BTC e saída: %f BTC 🚨' % (
-                        operation[0][3], opportunity[3]))
+                        trade_info[0][3], opportunity[3]))
 
             logger.run('🥂 Current balance: US$ %f 🥂' % actual_balance)
             in_trade = False
